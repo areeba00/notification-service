761a795b168b65a237d4efda92c1d40f7388fdec
areebakhawar <areebaakhawar@gmail.com>
2023-09-06 00:47:08 +0500

    error fixed

--- src/components/Applications/Card/Card.tsx
+++ src/components/Applications/Card/Card.tsx
@@ -1,6 +1,5 @@
 import Button from "@mui/material/Button";
 import { RiRadioButtonLine } from "react-icons/ri";
-import ActionButtonGroup from "../../../common/ActionButtonGroup/ActionButtonGroup";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
@@ -9,57 +8,50 @@ import TextField from "@mui/material/TextField";
 import { useState, ChangeEvent } from "react";
 
 import "./Cards.css";
-
-
-
+import ActionButtonGroup from "../../../common/ActionButtonGroup/ActionButtonGroup";
 
 interface Applications {
-    id: number;
-    name: string;
-    description: string;
-    created_at: string;
-    updated_at: string;
-    isActive: boolean;
-  }
-
-interface Props {
-    deleteApplication: (app: Applications) => void;
-    applications: Applications;
+  id: number;
+  name: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+  isActive: boolean;
 }
 
-
-
+interface Props {
+  deleteApplication: (app: Applications) => void;
+  applications: Applications;
+}
 
 const Cards = (app: Props) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [formData, setFormData] = useState({
-      name: "",
-      description: "",
-    });
-    const openModal = () => {
-      setIsModalOpen(true);
-    };
-  
-    const closeModal = () => {
-      setIsModalOpen(false);
-    };
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target;
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    };
-  
-    const handleSave = () => {
-      closeModal();
-    };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    description: "",
+  });
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSave = () => {
+    closeModal();
+  };
 
   return (
-        <article className="card">
-          {/* <div
+    <article className="card">
+      {/* <div
             className="thumb"
             style={{
               backgroundImage:
@@ -67,74 +59,74 @@ const Cards = (app: Props) => {
             }}
           ></div> */}
 
-          <div className="C-infos" key={app.applications.id}>
-            <h2 className="C-title">{app.applications.name}</h2>
-            <h3 className="C-status">
-              Status: &ensp; <RiRadioButtonLine className="TB-radio-button" />
-            </h3>
-            <div className="text-center">
-              <ActionButtonGroup
-                onEditClick={openModal}
-                onDeleteClick={() => app.deleteApplication(app.applications)}
-                // onToggleClick={() => toggleApplication(app)}
-                isActive={app.applications.isActive}
-              />
-            </div>
-            <br></br>
-            {/* <h3 className="seats">the details</h3> */}
-            <p className="C-txt">
-              {/* {app.description} */} Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Totam, voluptatum!
-            </p>
-          </div>
-          <Dialog
-            open={isModalOpen}
-            onClose={closeModal}
-            PaperProps={{
-              style: {
-                width: "90%",
-                maxWidth: "600px",
-                height: "60vh",
-                maxHeight: "600px",
-                borderRadius: "10px",
-              },
-            }}
-          >
-            <DialogTitle style={{ textAlign: "center", marginTop: "20px" }}>
-              Edit Application
-            </DialogTitle>
-            <DialogContent style={{ padding: "26px" }}>
-              <TextField
-                label="Name"
-                name="name"
-                fullWidth
-                margin="dense"
-                variant="outlined"
-                style={{ marginBottom: "30px", marginTop: "10px" }}
-                value={formData.name}
-                onChange={handleInputChange}
-              />
-              <TextField
-                label="Description"
-                name="description"
-                fullWidth
-                margin="dense"
-                variant="outlined"
-                // style={{ marginBottom: "20px" }}
-                value={formData.description}
-                onChange={handleInputChange}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={closeModal} color="secondary">
-                Cancel
-              </Button>
-              <Button onClick={handleSave} color="primary">
-                Save
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </article>
+      <div className="C-infos" key={app.applications.id}>
+        <h2 className="C-title">{app.applications.name}</h2>
+        <h3 className="C-status">
+          Status: &ensp; <RiRadioButtonLine className="TB-radio-button" />
+        </h3>
+        <div className="text-center">
+          <ActionButtonGroup
+            onEditClick={openModal}
+            onDeleteClick={() => app.deleteApplication(app.applications)}
+            // onToggleClick={() => toggleApplication(app)}
+            isActive={app.applications.isActive}
+          />
+        </div>
+        <br></br>
+        {/* <h3 className="seats">the details</h3> */}
+        <p className="C-txt">
+          {/* {app.description} */} Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Totam, voluptatum!
+        </p>
+      </div>
+      <Dialog
+        open={isModalOpen}
+        onClose={closeModal}
+        PaperProps={{
+          style: {
+            width: "90%",
+            maxWidth: "600px",
+            height: "60vh",
+            maxHeight: "600px",
+            borderRadius: "10px",
+          },
+        }}
+      >
+        <DialogTitle style={{ textAlign: "center", marginTop: "20px" }}>
+          Edit Application
+        </DialogTitle>
+        <DialogContent style={{ padding: "26px" }}>
+          <TextField
+            label="Name"
+            name="name"
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            style={{ marginBottom: "30px", marginTop: "10px" }}
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+          <TextField
+            label="Description"
+            name="description"
+            fullWidth
+            margin="dense"
+            variant="outlined"
+            // style={{ marginBottom: "20px" }}
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeModal} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleSave} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </article>
   );
 };
 
